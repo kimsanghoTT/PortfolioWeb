7fc5c36fc6b63c623441739e45215416499ac366
kimsangho <qwert8494@naver.com>
2025-08-06 10:44:23 +0900

    프로젝트 디테일

--- src/components/pages/project_detail/index.tsx
+++ src/components/pages/project_detail/index.tsx
@@ -3,6 +3,7 @@ import { useParams } from "next/navigation";
 import projectList from "../project/data.json";
 import styles from "./projectDetail.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface Project {
     id: string;
@@ -20,65 +21,75 @@ interface Project {
 const ProjectDetail = () => {
     const projectId = useParams().id;
     const project:Project | undefined = projectList.find(item => item.id === projectId);
+    const router = useRouter();
 
     if (!project) {
         return <div className={styles.notFound}>프로젝트를 찾을 수 없습니다.</div>;
+    }
+
+    const backToProject = () => {
+        router.back();
     }
 
     return(
         <div className={styles.wrapper} style={{background: `linear-gradient(to right, #fff 40%, transparent), url(${project.image}) no-repeat center/cover`}}>
             <section className={styles.container}>
                 <div className={styles.upperArea}>
-                    <span className={styles.back}><span className="hidden">뒤로가기</span></span>
+                    <span className={styles.back} onClick={backToProject}><span className="hidden">뒤로가기</span></span>
                 </div>
                 <div className={styles.contentBox}>
                     <h2 className={styles.projectTitle}>
                         {project.title}
                     </h2>
-                    <div className={styles.description}>
+                    <div className={`${styles.description} ${styles.listBlock}` }>
                         <span className={styles.title}>Description</span>
                         <p>{project.description}</p>
                     </div>
-                    <div className={styles.usedSkills}>
+                    <div className={`${styles.usedSkills} ${styles.listBlock}`}>
                         <span className={styles.title}>Used Skills</span>
+
+                        <span className={styles.subtitle}>Languages</span>
                         <ul className={styles.languages}>
-                            <span className={styles.title}>Languages</span>
                             {project.languages.map((item, index) => (
                                 <li key={index}>{item}</li>
                             ))}
                         </ul>
+
+                        <span className={styles.subtitle}>Framework / Library</span>
                         <ul className={styles.frameworks}>
-                            <span className={styles.title}>Framework / Library</span>
                             {project.frameworks.map((item, index) => (
                                 <li key={index}>{item}</li>
                             ))}
                         </ul>
+                        
+                        <span className={styles.subtitle}>DB</span>
                         <ul className={styles.dbs}>
-                            <span className={styles.title}>DB</span>
                             <li>{project.DB}</li>
                         </ul>
                     </div>
-                    <ul className={styles.linkList}>
+                    <div className={`${styles.linkList} ${styles.listBlock}`}>
                         <span className={styles.title}>References</span>
-                        {project.link !== "" && (
-                            <div className={styles.link}>
-                                <span className={styles.title}>Link</span>
-                                <Link href={project.link} target="_blank">{project.link}</Link>
-                            </div>
-                        )}
-                        {project.git !== "" && (
-                            <div className={styles.link}>
-                                <span className={styles.title}>Github</span>
-                                <Link href={project.git} target="_blank">{project.git}</Link>
-                            </div>                       
-                        )}
-                        {project.notion !== "" && (
-                            <div className={styles.link}>
-                                <span className={styles.title}>Detail</span>
-                                <Link href={project.notion} target="_blank">{project.notion}</Link>
-                            </div>
-                        )}                        
-                    </ul>
+                        <ul>
+                            {project.link !== "" && (
+                                <li className={styles.link}>
+                                    <span className={styles.subtitle}>Link</span>
+                                    <Link href={project.link} target="_blank">{project.link}</Link>
+                                </li>
+                            )}
+                            {project.git !== "" && (
+                                <li className={styles.link}>
+                                    <span className={styles.subtitle}>Github</span>
+                                    <Link href={project.git} target="_blank">{project.git}</Link>
+                                </li>                       
+                            )}
+                            {project.notion !== "" && (
+                                <li className={styles.link}>
+                                    <span className={styles.subtitle}>Detail</span>
+                                    <Link href={project.notion} target="_blank">{project.notion}</Link>
+                                </li>
+                            )}      
+                        </ul>
+                    </div>
                 </div>
             </section>
         </div>
