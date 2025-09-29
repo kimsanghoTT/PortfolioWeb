a2e036f7477fa8b3a670e2b4d71012c1cdb8c23e
kimsangho <qwert8494@naver.com>
2025-09-29 16:33:19 +0900

    스크롤 가이드

--- src/components/pages/project/sub_components/project_board.tsx
+++ src/components/pages/project/sub_components/project_board.tsx
@@ -83,8 +83,7 @@ const ProjectBoard = () => {
                         </button>
                     ))}
                 </div>
-                <span className={styles.boardNotice}>
-                    <span>Scroll or Drag</span>
+                <span className={styles.mobileDragGuide}>
                     <svg width="150" height="20" xmlns="http://www.w3.org/2000/svg">
                         <line x1="0" y1="10" x2="80" y2="10" stroke-width="2"
                                 stroke-dasharray="6,6" />
